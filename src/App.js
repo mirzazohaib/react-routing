50bda7f92e1355a7ae8fb0fed6dd90f5b28d4be5
Zohaib <54810131+mirzazohaib@users.noreply.github.com>
2022-08-29 16:39:30 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/contact">Contact</Link>{' '}
+          <Link to="/about">About</Link>{' '}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
