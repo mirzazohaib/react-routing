951ffe8b8514381b835fe765650771e5bb188f1c
Zohaib <54810131+mirzazohaib@users.noreply.github.com>
2022-08-31 17:21:40 +0300

    auth0 added
    Complete User Registration, Login & Logout Functionality in React with Auth0

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import Button from "@mui/material/Button";
+
+//Import the useAuth0 hook from @auth0/auth0-react to get the methods
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Home() {
-  return( 
+  //get all the neccessary methods from the useAuth0 Hook
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
+
+  return (
+    //to toggle the login and logout button
     <div>
-      <h1>Welcome to my homepage</h1>
+      {isAuthenticated && <h1>Welcome to my homepage: {user.name}</h1>}
+
+      {isAuthenticated ? (
+        <Button
+          variant="outlined"
+          color="error"
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Log Out
+        </Button>
+      ) : (
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
     </div>
-  )
+  );
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+  domain="dev-b44p8.auth0.com"
+  clientId="B5MPAiPbnGl1jpa70yFK6IXNj4CDdA9M"
+  redirectUri={window.location.origin}
+>
+
     <App />
-  </React.StrictMode>
+    </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
